1a523646171cdc4b79f5b2ff467500a1b29e3842
Boubacar Diallo <90484140+boubabaueel@users.noreply.github.com>
2024-09-06 21:34:12 -0400

    created navbar in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-  <main></main>
+  <main className="font-light text-white antialiased
+   selection:bg-lime-300 selection:text-black">
+    <Navbar/>
+     </main>
   )
 }
 
 export default App
+  
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react"
+import { FaBars, FaTimes } from "react-icons/fa"
 
 const Navbar = () => {
+  const[isOpen, setIsOpen] = useState(false)
+  const toggleMenu = () =>{
+    setIsOpen(!isOpen)
+  }
+  useEffect(() => {
+    if(!isOpen){
+      document.body.style.overflow ="hidden";
+
+    }
+    
+  else { 
+    document.body.style.overflow = "auto";
+  }
+
+  }, [isOpen])
   return (
-    <div>
-      
-    </div>
+    <nav className="fixed right-0 top-0 z-30 p-4">
+      <button onClick={toggleMenu} className="rounded-md p-2">
+        {isOpen ? (
+          <FaTimes className="h-6 w-6"/>
+        ) : (
+          <FaBars className="h-6 w-6"/>
+        )} </button>
+    </nav>
   )
 }
 
